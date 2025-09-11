35ccf752aee2fa92f16fca282cf1c35cf6f25ed6
yjst0702 <yjst0702@gmail.com>
2025-09-11 17:48:06 +1000

    hamburger nav + menu

---
+++ script.js
@@ -0,0 +1,31 @@
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+const overlay = document.querySelector("#overlay");
+const nav1 = document.querySelector("#nav-1");
+const nav2 = document.querySelector("#nav-2");
+const nav3 = document.querySelector("#nav-3");
+const nav4 = document.querySelector("#nav-4");
+const nav5 = document.querySelector("#nav-5");
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+// 네비게이션 애니메이션
+function navAnimation(val1, val2) {
+  navItems.forEach((nav, i) => {
+    nav.classList.replace(`slide-${val1}-${i + 1}`, `slide-${val2}-${i + 1}`);
+  });
+}
+
+function toggleNav() {
+  hamburgerMenu.classList.toggle("active");
+  overlay.classList.toggle("overlay-active");
+
+  if (overlay.classList.contains("overlay-active")) {
+    overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+    navAnimation("out", "in");
+  } else {
+    overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+    navAnimation("in", "out");
+  }
+}
+
+hamburgerMenu.addEventListener("click", toggleNav);
+navItems.forEach((nav) => nav.addEventListener("click", toggleNav));
